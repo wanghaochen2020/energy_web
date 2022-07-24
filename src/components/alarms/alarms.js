56df5426d58000a98715eb550b95f000b1615582
wangjinglei <wangqingshui1985@163.com>
2022-07-24 21:57:29 +0800

    update alarm style

--- src/components/alarms/alarms.js
+++ src/components/alarms/alarms.js
@@ -4,66 +4,92 @@ import { Popover, Button } from 'antd';
 import './alarms.scss';
 
 export const ComAlarms = ({ items }) => {
-  const [popoverVisible, setPopoverVisible] = useState(false);
-  const [popoverVisible2, setPopoverVisible2] = useState(false);
   // if (!items) {
   //   return null;
   // }
 
+  const [data, setData] = useState([
+    {
+      order: 1,
+      name: '智能设备1',
+      type: '故障',
+      time: '2022-07-24',
+      status: '未处理'
+    },
+    {
+      order: 2,
+      name: '智能设备2',
+      type: '故障',
+      time: '2022-07-24',
+      status: '未处理'
+    },
+    {
+      order: 3,
+      name: '智能设备3',
+      type: '故障',
+      time: '2022-07-24',
+      status: '已处理'
+    },
+    {
+      order: 4,
+      name: '智能设备4',
+      type: '故障',
+      time: '2022-07-24',
+      status: '未处理'
+    },
+    {
+      order: 5,
+      name: '智能设备5',
+      type: '故障',
+      time: '2022-07-24',
+      status: '已处理'
+    }
+  ]);
+
+  const setPopoverVisible = (item, val, status) => {
+    item.popoverVisible = val;
+    if (status) {
+      item.status = status;
+    }
+    setData([...data]);
+  };
+
+  const title = <thead><tr className="alarm-title">
+    <th>序号</th>
+    <th>名称</th>
+    <th>异常类型</th>
+    <th>故障时间</th>
+    <th>状态</th>
+  </tr></thead>
+
+  const rows = <tbody>{data.map((item, index) => 
+    <tr key={index}>
+      <td>{item.order}</td>
+      <td>{item.name}</td>
+      <td>{item.type}</td>
+      <td>{item.time}</td>
+      {item.status === '已处理' ? <td className="alarm-resolved">{item.status}</td>:
+        <td>
+          <Popover
+            content={<div className="popover-button-wrapper">
+              <Button type="primary" onClick={() => setPopoverVisible(item, false, '已处理')}>确定</Button>
+              <Button type="secondary" onClick={() => setPopoverVisible(item, false)}>取消</Button></div>}
+            title="确定要标记为已处理吗？"
+            trigger="click"
+            visible={item.popoverVisible}
+            onVisibleChange={(val) => setPopoverVisible(item, val)}
+          >
+          <span className="alarm-unresolved" onClick={() => setPopoverVisible(item, !item.popoverVisible)}>待处理</span>
+        </Popover></td>}
+    </tr>
+  )}</tbody>;
+
   return (
     <div className="com-alarms">
-      <div className="alarm-item">
-        <span className="alarm-level">严重</span>
-        <span className="alarm-content">
-          <span className="alarm-time">13:26</span>
-          严重警告
-        </span>
-        <Popover
-          content={<div className="popover-button-wrapper">
-            <Button type="primary" onClick={() => setPopoverVisible(false)}>确定</Button>
-            <Button type="secondary" onClick={() => setPopoverVisible(false)}>取消</Button></div>}
-          title="确定要标记为已处理吗？"
-          trigger="click"
-          visible={popoverVisible}
-          onVisibleChange={setPopoverVisible}
-        >
-        <span className="alarm-status" onClick={() => setPopoverVisible(!popoverVisible)}>待处理</span>
-      </Popover>
-      </div>
-      <div className="alarm-item">
-        <span className="alarm-level">严重</span>
-        <span className="alarm-content">
-          <span className="alarm-time">15:13</span>
-          严重警告
-        </span>
-        <Popover
-          content={<div className="popover-button-wrapper">
-            <Button type="primary" onClick={() => setPopoverVisible2(false)}>确定</Button>
-            <Button type="secondary" onClick={() => setPopoverVisible2(false)}>取消</Button></div>}
-          title="确定要标记为已处理吗？"
-          trigger="click"
-          visible={popoverVisible2}
-          onVisibleChange={setPopoverVisible2}
-        >
-        <span className="alarm-status" onClick={() => setPopoverVisible2(!popoverVisible2)}>待处理</span>
-      </Popover>
-      </div>
-      <div className="alarm-item">
-        <span className="alarm-level yellow-alarm">重要</span>
-        <span className="alarm-content">
-          <span className="alarm-time">16:24</span>
-          重要通知重要通知
-        </span>
-        <span className="alarm-status alarm-status-solved">处理完毕</span>
-      </div>
-      <div className="alarm-item">
-        <span className="alarm-level yellow-alarm">重要</span>
-        <span className="alarm-content">
-          <span className="alarm-time">18:45</span>
-          重要通知重要通知
-        </span>
-        <span className="alarm-status alarm-status-solved">处理完毕</span>
-      </div>
+      <table className="table-alarms">
+        {title}
+        {rows}
+      </table>
     </div>
   );
 }
